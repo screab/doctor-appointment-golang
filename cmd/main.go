eaaf7f590d348647c1303b1786ae10c37bc0599d
screab <1006313+screab@users.noreply.github.com>
2026-02-16 19:11:19 +0200

    Create cmd/main.go file to start the Go application for appointments API

--- cmd/main.go
+++ cmd/main.go
@@ -1,15 +1,32 @@
 package main
 
 import (
-    "log"
-    "net/http"
-    "screab/doctor-appointment-golang/api"
+	"database/sql"
+	"fmt"
+	"log"
+	"net/http"
+	_ "github.com/lib/pq"
 )
 
 func main() {
-    router := api.SetupRouter()
-    log.Println("Server is running on port 8080")
-    if err := http.ListenAndServe(":8080", router); err != nil {
-        log.Fatal(err)
-    }
+	connStr := "user=username dbname=appointments sslmode=test"
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	_, err = db.Exec(`CREATE TABLE IF NOT EXISTS appointment (id SERIAL PRIMARY KEY, title TEXT, date TIMESTAMP)`)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	http.HandleFunc("/appointments", appointmentsHandler)
+	log.Println("Starting server on :8080...")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func appointmentsHandler(w http.ResponseWriter, r *http.Request) {
+	// Logic to handle appointments goes here
+	fmt.Fprintln(w, "Appointments Endpoint")
 }
